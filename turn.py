95883348451cba785670915c15f5a0623bfadbb0
oopsuwu <60373320+oopsuwu@users.noreply.github.com>
2022-08-13 17:05:33 -0400

    imports
    I just decided to import Jake Belanger's entire repo into mine.

---
+++ turn.py
@@ -0,0 +1,11 @@
+from wizAPI import *
+import time
+import math
+player = wizAPI().register_window()
+
+player.hold_key('a', .55)
+player.hold_key('w', 2)
+
+
+    #go to the exit, by going to where the arrow is pointed and going forward
+    
